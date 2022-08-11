471bb2ecc3e144be72c980a260cdf3a1518cdadf
Mark <qudfbftnqls@naver.com>
2022-08-11 12:45:48 +0900

    [점프 투 스프링부트] JPA를 이용한 리포지터리 생성 및 데이터 처리

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,128 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class SbbApplicationTests {
-
+	
+	@Autowired
+	private QuestionRepository questionRepository;
+	@Autowired
+	private AnswerRepository answerRepository;
+	
 	@Test
-	void contextLoads() {
+	void save() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);	// 첫번째 질문 저장
+		
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);	// 두번째 질문 저장
 	}
-
+	
+	@Test
+	void findAll() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+		
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+	
+	@Test
+	void findById() {
+		Optional<Question> oq = this.questionRepository.findById(11);
+		if (oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+	}
+	
+	@Test
+	void findBySubject() {
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(11, q.getId());
+	}
+	
+	@Test
+	void findBySubjectAndContent() {
+		Question q = this.questionRepository.findBySubjectAndContent(
+				"sbb가 무엇인가요?", "sbb에 대해서 알고싶습니다.");
+		assertEquals(11, q.getId());
+	}
+	
+	@Test
+	void findBySubjectLike() {
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+	
+	@Test
+	void update() {
+		Optional<Question> oq = this.questionRepository.findById(11);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
+	}
+	
+	@Test
+	void delete() {
+		assertEquals(2,  this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(11);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+	}
+	
+	@Test
+	void saveAnswer() {
+		Optional<Question> oq = this.questionRepository.findById(12);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q);	// 어떤 질문의 답변인지 알기 위해서 Question 객체가 필요하다.
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
+	}
+	
+	@Test
+	void findAnswerById() {
+		Optional<Answer> oa = this.answerRepository.findById(1);
+		assertTrue(oa.isPresent());
+		Answer a = oa.get();
+		assertEquals(12, a.getQuestion().getId());
+	}
+	
+	@Transactional
+	@Test
+	void findAnswerByQuestion() {
+		Optional<Question> oq = this.questionRepository.findById(12);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		
+		List<Answer> answerList = q.getAnswerList();
+		
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
 }
